5ecd1eca1f92ee703a5f5c9c2a96635a9460948a
Sabri <sabri.ouaked.ext@exotec.com>
2023-12-05 10:32:53 +0100

    feat(ressources): adding teacher ressources

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 export default function App() {
 
+    const teachers = [
+        'Madame BOURRELI', 'Madame EL ALLOUCHI', 'Madame FLAMANT', 'Monsieur LESAINT', 'Madame MARIEN', 'Madame OUAKED', 'Monsieur RAGEOT', 'Monsieur SALAMA'
+    ]
     return (
         <div className="h-screen w-screen bg-blue-500 flex items-center justify-center">
-            <div className="w-full max-w-xl bg-white p-6 rounded-lg">
+            <div className="w-full h-full md:max-w-xl md:h-fit bg-white p-6 rounded-lg">
                 <h1 className="text-xl md:text-2xl font-bold uppercase">Dispositif Anti-Harcelement</h1>
                 
                 <div className="py-6">
@@ -10,8 +13,11 @@ export default function App() {
                         <label className="font-semibold text-l">Qui souhaitez vous prévenir ?</label>
                         <select className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                             <option selected>Choisir un professeur ressource</option>
-                            <option value="US">Leila Ouaked</option>
-                            <option value="CA">Professeur Layton</option>
+                            {
+                                teachers.map((teacher: string) => {
+                                    return <option value={teacher}>{teacher}</option>
+                                })
+                            }
                         </select>
                     </div>
 
@@ -24,6 +30,7 @@ export default function App() {
                         <label className="block mb-2 text-sm font-medium text-gray-900 ">Avez vous des preuves à ajouter?</label>
                         <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none " id="multiple_files" type="file" multiple />
                     </div>
+
                 </div>
 
                 <button className="bg-blue-500 font-semibold text-white uppercase w-full py-1.5 rounded-lg">
